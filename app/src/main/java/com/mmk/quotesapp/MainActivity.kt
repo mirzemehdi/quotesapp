bc05d8258fe26d4f2aea56783e8f25d941e3264a
mirzemehdi <mirzemehdi@gmail.com>
2020-07-11 21:49:14 +0400

    Initial Commit

---
+++ app/src/main/java/com/mmk/quotesapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mmk.quotesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
