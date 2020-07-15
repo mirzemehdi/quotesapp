f9359260b30c036ac4dcc6af6c8b38f96b76c3ae
mirzemehdi <mirzemehdi@gmail.com>
2020-07-15 23:46:55 +0400

    Adding DataBinding

--- app/src/main/java/com/mmk/quotesapp/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/mmk/quotesapp/ui/fragments/main/MainFragment.kt
@@ -2,23 +2,38 @@ package com.mmk.quotesapp.ui.fragments.main
 
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.mmk.quotesapp.R
+import com.mmk.quotesapp.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_main.*
 
 @AndroidEntryPoint
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment() {
     val viewModel:MainViewHolder by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding=FragmentMainBinding.inflate(inflater,container,false)
+        context?: return binding.root
+        binding.lifecycleOwner=this
+        binding.input="Salam"
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        observeValues()
+       // observeValues()
     }
 
     private fun observeValues() {
