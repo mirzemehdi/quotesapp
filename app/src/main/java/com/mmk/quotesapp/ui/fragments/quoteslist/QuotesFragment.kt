1403f6f65a8e310ea7994cc36691d2667b0b8b07
mirzemehdi <mirzemehdi@gmail.com>
2020-08-02 23:31:40 +0400

    Adding Bottom Navigation

---
+++ app/src/main/java/com/mmk/quotesapp/ui/fragments/quoteslist/QuotesFragment.kt
@@ -0,0 +1,8 @@
+package com.mmk.quotesapp.ui.fragments.quoteslist
+
+import androidx.fragment.app.Fragment
+import com.mmk.quotesapp.R
+
+class QuotesFragment: Fragment(R.layout.fragement_quotes) {
+
+}
