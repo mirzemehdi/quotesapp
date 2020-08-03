568bd4cfdc135ed45637022c1260bbe0341f9baa
mirzemehdi <mirzemehdi@gmail.com>
2020-08-04 01:55:58 +0400

    Adding Firebase

--- app/src/main/java/com/mmk/quotesapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/mmk/quotesapp/ui/activities/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //throw RuntimeException("Test Crash") // Force a crash
 
 
     }
