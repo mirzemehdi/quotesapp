69886011af9fcf6751b814f16cbefb6de8c7a38a
Mirzemehdi <mirzemehdi@gmail.com>
2021-07-13 01:00:00 +0400

    Implementing UI tests for QuotesFragment

--- app/src/androidTest/java/com/mmk/quotesapp/ui/fragments/quoteslist/QuotesFragmentTest.kt
+++ app/src/androidTest/java/com/mmk/quotesapp/ui/fragments/quoteslist/QuotesFragmentTest.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.paging.PagingData
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
@@ -17,6 +18,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.mmk.domain.model.Quote
 import com.mmk.quotesapp.R
 import com.mmk.quotesapp.base.BaseTestApp
+import com.mmk.quotesapp.ui.base.UiState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
@@ -29,6 +31,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.koin.core.context.unloadKoinModules
 import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.mockito.Mock
@@ -49,23 +52,53 @@ class QuotesFragmentTest : KoinTest {
     @Before
     fun setup() {
         viewModel = Mockito.mock(QuotesViewModel::class.java)
-        loadKoinModules(module { viewModel { viewModel } })
-        whenever(viewModel.quotesListPagedData).thenReturn(MutableLiveData(PagingData.empty()))
-        scenario = launchFragmentInContainer(themeResId = R.style.AppTheme)
-        scenario.moveToState(newState = Lifecycle.State.STARTED)
-
+        loadKoinModules(module { viewModel(override = true) { viewModel } })
     }
+
 
     @Test
     fun emptyView_isShownInUi_whenThereIsNoQuote() {
+        whenever(viewModel.quotesListPagedData).thenReturn(MutableLiveData(PagingData.empty()))
+        whenever(viewModel.uiState).thenReturn(MutableLiveData(UiState.NoData))
+        scenario = launchFragmentInContainer(themeResId = R.style.AppTheme)
+        scenario.moveToState(newState = Lifecycle.State.STARTED)
         onView(withId(R.id.emptyView)).check(matches(isDisplayed()))
     }
 
+    @Test
+    fun data_isShownInUi_whenThereAreQuotes() {
+        val quoteList = listOf(
+            Quote(id = "1", author = "Test1", text = "Text"),
+            Quote(id = "2", author = "Test2", text = "Text")
+        )
+        whenever(viewModel.quotesListPagedData)
+            .thenReturn(MutableLiveData(PagingData.from(quoteList)))
+        whenever(viewModel.uiState).thenReturn(MutableLiveData(UiState.HasData))
+        scenario = launchFragmentInContainer(themeResId = R.style.AppTheme)
+        scenario.moveToState(newState = Lifecycle.State.STARTED)
+        onView(withId(R.id.emptyView)).check(
+            matches(
+                ViewMatchers.withEffectiveVisibility(
+                    ViewMatchers.Visibility.GONE
+                )
+            )
+        )
+        onView(withId(R.id.quotesRecyclerView)).check(
+            matches(
+                ViewMatchers.withEffectiveVisibility(
+                    ViewMatchers.Visibility.VISIBLE
+                )
+            )
+        )
 
-    @After
-    fun cleanUp() {
-        stopKoin()
+        onView(withId(R.id.quotesRecyclerView)).check { view, noViewFoundException ->
+            view as RecyclerView
+            assert(view.adapter?.itemCount == quoteList.size)
+        }
     }
+
+
+
 
 
 }
