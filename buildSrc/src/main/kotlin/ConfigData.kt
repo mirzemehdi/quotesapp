ec8a5fb1dd1beaf01e08043c3975ebd581c20bc6
Mirzamehdi <mirzemehdi@gmail.com>
2022-09-26 13:33:40 +0200

    Adding ItemQuoteList view

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,8 +1,8 @@
 object ConfigData {
 
     const val appId = "com.mmk.quotesapp"
-    const val compileSdkVersion = 31
-    const val targetSdkVersion = 31
+    const val compileSdkVersion = 32
+    const val targetSdkVersion = 32
     const val minSdkVersion = 21
     const val versionCode = 1
     const val versionName = "1.0.0"
