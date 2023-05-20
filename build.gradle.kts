8d2f2d07e2aadf865de2c19ec0ac093025501ad1
Mirzamehdi <mirzemehdi@gmail.com>
2023-05-20 04:10:20 +0200

    Creating empty shared module

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.3.3")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     }
 }
 
