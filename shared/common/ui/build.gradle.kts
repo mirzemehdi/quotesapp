730fb46963ea38471085be1e005782552f00f2c5
Mirzamehdi <mirzemehdi@gmail.com>
2023-05-27 06:17:50 +0200

    Adding common ui resources

--- shared/common/ui/build.gradle.kts
+++ shared/common/ui/build.gradle.kts
@@ -1,3 +1,14 @@
 plugins {
     id(Plugins.multiPlatformComposePlugin)
+    id(Plugins.multiPlatformResources)
+
+}
+
+dependencies {
+    commonMainApi(Libs.mokoResourcesCompose) // for compose multiplatform
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = "com.mmk.common.ui"
+    multiplatformResourcesVisibility = dev.icerock.gradle.MRVisibility.Internal // optional, default Public
 }
