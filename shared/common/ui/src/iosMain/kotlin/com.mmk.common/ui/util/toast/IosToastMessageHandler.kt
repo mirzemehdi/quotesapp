72ff92bd0cc1c1ce9a766e3c83fc023cf9c83a0b
Mirzamehdi <mirzemehdi@gmail.com>
2023-06-01 19:32:54 +0200

    Providing mock implementations for iosMain for now

--- shared/common/ui/src/iosMain/kotlin/com.mmk.common/ui/util/toast/IosToastMessageHandler.kt
+++ shared/common/ui/src/iosMain/kotlin/com.mmk.common/ui/util/toast/IosToastMessageHandler.kt
@@ -2,18 +2,18 @@ package com.mmk.common.ui.util.toast
 
 import com.mmk.common.ui.util.UiMessage
 
+// TODO implement toast message in ios
 class IosToastMessageHandler : ToastMessageHandler {
     override fun showToast(message: String?, duration: ToastMessageHandler.Duration) {
         message?.let {
-            TODO("Not implemented yet")
+            println("ios toast message: $message")
         }
     }
 
     override fun showToast(uiMessage: UiMessage, duration: ToastMessageHandler.Duration) {
-        TODO("Not implemented yet")
-        val messageText = when (uiMessage) {
-            is UiMessage.Message -> uiMessage.message ?: ""
-            is UiMessage.Resource -> ""
-        }
+//        val messageText = when (uiMessage) {
+//            is UiMessage.Message -> uiMessage.message ?: ""
+//            is UiMessage.Resource -> ""
+//        }
     }
 }
--- shared/root/src/iosMain/kotlin/main.ios.kt
+++ shared/root/src/iosMain/kotlin/main.ios.kt
@@ -1,13 +1,9 @@
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.window.ComposeUIViewController
+import com.mmk.root.RootScreen
 import platform.UIKit.UIViewController
 
-@Composable
-internal fun TestView() {
-    Text("Hellooooo")
-}
 fun MainViewController(): UIViewController {
 
-    return ComposeUIViewController { TestView() }
+    return ComposeUIViewController { RootScreen() }
 }
