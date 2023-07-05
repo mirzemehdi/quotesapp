53b2bcbff98bc687eec429fb0d7445fa633a5439
Mirzamehdi <mirzemehdi@gmail.com>
2023-07-06 01:18:13 +0200

    Fixing CI/CD

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
     mavenCentral()
     google()
     gradlePluginPortal()
-//    maven("https://plugins.gradle.org/m2/")
+    maven("https://plugins.gradle.org/m2/")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev/")
 }
 
